f876517a7764c744c9d3879efad0cf41c9b0f198
Yevhenii Korolikhin <yevheniikorolihin@gmail.com>
2025-02-20 18:43:15 +0200

    remove unused commands

--- src/client/story-render/command/update-progress.command.ts
+++
@@ -1,8 +0,0 @@
-import { CommandInterface } from "@/generic/cqrs/command/interface/command.interface";
-import { StoryRenderProgressInterface } from "@/client/story-render/interface/story-render-progress.interface";
-
-export class UpdateProgressCommand implements CommandInterface{
-    constructor(
-        public readonly progress: StoryRenderProgressInterface
-    ){}
-}
